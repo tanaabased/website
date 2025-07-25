e15fc0f7db5f56d47f5dcd9941abb96b9e9bc8b9
Mike Pirog <mike@lando.dev>
2025-07-25 15:30:44 -0400

    initial payload of stuff

--- docs/.vitepress/config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vitepress'
-import theme from '@lando/vitepress-theme-default-plus'
-
-export default defineConfig({
-  extends: theme,
-  lang: 'en-US',
-  title: 'TMS Website',
-  description: 'Main marketing site',
-})
